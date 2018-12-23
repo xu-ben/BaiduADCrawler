974972096a99a745dc5e33aee35f1d50e0fa5f6c
Ben Xu <xuben.cn@gmail.com>
2018-12-23 21:20:44 +0800

    use gson

--- src/main/java/ben/tools/crawler/ZMProxy.java
+++ src/main/java/ben/tools/crawler/ZMProxy.java
@@ -1,20 +1,30 @@
 package ben.tools.crawler;
 
 import ben.tools.crawler.bdadcrawler.City;
+import com.google.gson.Gson;
 
 import java.io.IOException;
 
 public class ZMProxy {
 
+	/**
+	 * 调用芝麻代理api返回的结果
+	 */
+	private class Result {
+		String msg;
+		boolean success;
+		int code;
+	}
+
 	private static String FREE_URL = "http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=%d&city=%d&yys=0&port=1&pack=37000&ts=0&ys=0&cs=0&lb=4&sb=0&pb=4&mr=1&regions=";
 
 	private static String FIVE_URL = "http://webapi.http.zhimacangku.com/getip?num=1&type=1&pro=%d&city=%d&yys=0&port=1&time=1&ts=0&ys=0&cs=0&lb=4&sb=0&pb=4&mr=1&regions=";
 
-	public static String fetchProxyFromServer1(City city) throws IOException {
+	public static String fetchProxyFromServer(City city) throws IOException {
 		return fetchProxyFromServer(FREE_URL, city, 3);
 	}
 
-	public static String fetchProxyFromServer(City city) throws IOException {
+	public static String fetchProxyFromServer2(City city) throws IOException {
 		return fetchProxyFromServer(FIVE_URL, city, 3);
 	}
 
@@ -23,8 +33,16 @@ public class ZMProxy {
 		String cmd = String.format("curl -A \"%s\" \"%s\"", Commons.chromeUserAgent, url);
 		for (int i = 0; i < mosttry; i++) { // 最多尝试mosttry次
 			System.err.println(cmd);
-			String proxy = Commons.execCmdInDir(cmd, ".", 5);
-			if (proxy.matches("\\s*\\d+\\.\\d+\\.\\d+\\.\\d+:\\d+\\s*")) {
+			String proxy = Commons.execCmdInDir(cmd, ".", 15);
+			if (proxy.charAt(0) == '{') {
+				Gson gson = new Gson();
+				Result res = gson.fromJson(proxy, Result.class);
+				System.out.println(res.msg);
+				// todo 添加更多逻辑
+				if (res.msg.equals("您的该套餐已过期!")) {
+					return null;
+				}
+			} else if (proxy.matches("\\s*\\d+\\.\\d+\\.\\d+\\.\\d+:\\d+\\s*")) {
 				System.err.println(proxy);
 				return proxy.trim();
 			} else {
